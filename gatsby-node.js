b6aa3e0b3ad2e782f434e70b4d33cfd23bfbac2d
Irfan Kurniawan <irfan@unity.sg>
2022-02-25 11:48:19 +0700

    publish

---
+++ gatsby-node.js
@@ -0,0 +1,24 @@
+exports.onCreatePage = async ({ page, actions }) => {
+    const { createPage } = actions
+
+    if (page.path.match(/^\/account/)) {
+      page.matchPath = "/account/*"
+  
+      createPage(page)
+    }
+  }
+
+  exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /auth0-js/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
